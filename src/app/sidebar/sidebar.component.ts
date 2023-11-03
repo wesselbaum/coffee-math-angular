e43a16c98ed4663a594635de49d1a5afdb35a8b1
Aleksej Wesselbaum <aleksej.wesselbaum@codecentric.de>
2023-11-03 12:52:26 +0100

    refactor: replaced EventEmitter with Subject

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,14 +1,22 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Recipe } from '../shared/recipe.model';
 import { RecipeService } from '../shared/services/recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [];
+  recipeServiceSubscription: Subscription | undefined;
 
   @Output() recipeClicked = new EventEmitter<undefined>();
 
@@ -16,9 +24,14 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.recipeToggledFavorite.subscribe(() => {
-      this.recipes = this.recipeService.getRecipes();
-    });
+    this.recipeServiceSubscription =
+      this.recipeService.recipeToggledFavorite.subscribe(() => {
+        this.recipes = this.recipeService.getRecipes();
+      });
+  }
+
+  ngOnDestroy() {
+    this.recipeServiceSubscription?.unsubscribe();
   }
 
   onRecipeClick() {
